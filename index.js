8322413b1887278c27875b90cbe71385482b1791
Arvida Jimenez <arvida.jimenez@gmail.com>
2019-12-26 02:52:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function leashDog(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
 }
 
+function walkToPark(dogName, dogBreed) {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+}
+
 
 
 
