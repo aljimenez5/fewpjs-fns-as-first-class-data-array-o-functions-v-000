4d9f7454f645956766c5323f197ba6d90cfd0b15
Arvida Jimenez <arvida.jimenez@gmail.com>
2019-12-26 02:53:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,5 @@ function throwFrisbee(dogName, dogBreed) {
 
 
 
-console.log("Leash Byron the poodle");
-console.log("Walk to the park with Byron the poodle");
-console.log("Throw the frisbee for Byron the poodle");
 console.log("Walk home with Byron the poodle");
 console.log("Unleash Byron the poodle");
