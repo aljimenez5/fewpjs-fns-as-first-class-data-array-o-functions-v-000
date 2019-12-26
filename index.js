4787ee0b3c5e515170c1ef05ccca08565978a44b
Arvida Jimenez <arvida.jimenez@gmail.com>
2019-12-26 03:03:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ function unleashDog(dogName, dogBreed) {
 const routine = [wakeDog(dogName, dogBreed), leashDog(dogName, dogBreed), walkToPark(dogName, dogBreed),throwFrisbee(dogName, dogBreed), walkHome(dogName, dogBreed), unleashDog(dogName, dogBreed)];
 
 function exerciseDog(dogName, dogBreed) {
-  var returnedMessages = []  
+  var returnedMessages = [];
+  
 }
 
 
